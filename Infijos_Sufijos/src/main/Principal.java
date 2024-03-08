dfcdb02a611d364560687ac111368f506c89e4d8
Lokcis <lokcis@hotmail.com>
2024-03-07 23:22:20 -0500

    Estructura basica

--- Infijos_Sufijos/src/main/Principal.java
+++ Infijos_Sufijos/src/main/Principal.java
@@ -1,10 +1,31 @@
-
 package main;
-
+import arrays.*;
+import java.util.Scanner;
 /**
  *
  * @author lokci
  */
 public class Principal {
+    
+    ArrayQueue<String> queueIn, queueOut;
+    ArrayBag<String> bag;
+    ArrayStack<String> stack;
+    
+    public Principal(){
+        queueIn = new ArrayQueue<>();
+        queueOut = new ArrayQueue<>();
+        bag = new ArrayBag<>();
+        stack = new ArrayStack<>();
+    }
+    
+    public static void main(String[] args) {
+        Principal main;
+        String ecu;
+        Scanner in = new Scanner(System.in);
+        ecu = in.next();
+        while(!ecu.isEmpty()){
+            
+        }
+    }
     
 }
